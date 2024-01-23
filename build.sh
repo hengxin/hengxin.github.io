54316278ec61beb7d6adca81cb58400dd6ae566b
root <root@hfwei>
2024-01-23 20:09:08 +0800

    update files: ./teaching.jemdoc

--- build.sh
+++ build.sh
@@ -2,9 +2,15 @@
 
 dir="."
 
+files="update files: "
 for file in "$dir"/*.jemdoc; do
     if [[ "$file" -nt "${file%.jemdoc}.html" ]]; then
+        files+="$file "
         python2 jemdoc.py "$file"
         echo "Compiled $file"
     fi
 done
+
+git add .
+git commit -m "$files"
+git push origin master
